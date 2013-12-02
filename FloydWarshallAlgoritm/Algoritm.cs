5b3e648832390d8b1372223b38c750243401f882
rem8086 <rem8086@gmail.com>
2013-12-03 00:15:40 +0400

    First work commit

---
+++ FloydWarshallAlgoritm/Algoritm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FloydWarshallAlgoritm
+{
+    class Algoritm
+    {
+        AdjacencyMatrix previousMatrix;
+        AdjacencyMatrix nextMatrix;
+        
+        public Algoritm(AdjacencyMatrix inputMatrix)
+        {
+            previousMatrix = (AdjacencyMatrix)inputMatrix.Clone();
+            nextMatrix = (AdjacencyMatrix)inputMatrix.Clone();
+        }
+
+        public void AlgoritmStep()
+        {
+            for (int k = 0; k < previousMatrix.NodeCount; k++)
+            {
+                for (int i = 0; i < previousMatrix.NodeCount; i++)
+                {
+                    for (int j = 0; j < previousMatrix.NodeCount; j++)
+                    {
+                        if ((previousMatrix.GetElement(i,k).HasValue)&&(previousMatrix.GetElement(k,j).HasValue))
+                        {
+                            int prev = previousMatrix.GetElement(i,j) ?? Int32.MaxValue;
+                            int next = previousMatrix.GetElement(i,k).Value + previousMatrix.GetElement(k,j).Value;
+                            nextMatrix.SetElement(i,j, prev <= next ? prev : next); 
+                        }
+                    }
+                }
+                previousMatrix = (AdjacencyMatrix)nextMatrix.Clone();
+            }
+        }
+
+        public string ShowResult()
+        {
+            return nextMatrix.ToString();
+        }
+    }
+}
---
+++ FloydWarshallAlgoritm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FloydWarshallAlgoritm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FloydWarshallAlgoritm")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ef78672f-6138-4d16-aede-ea093bb31d33")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
