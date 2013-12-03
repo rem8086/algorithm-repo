60bf24f8aca068c9265e726f5399ccf8a0c67dee
rem8086 <rem8086@gmail.com>
2013-12-03 08:53:14 +0400

    First work commit

---
+++ FloydWarshallAlgoritm/AdjacencyMatrix.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FloydWarshallAlgoritm 
+{
+    public class AdjacencyMatrix : ICloneable
+    {
+        int?[][] matrix;
+        int nodesCount;
+
+        public AdjacencyMatrix(int count)
+        {
+            matrix = new int?[count][];
+            nodesCount = count;
+            for (int i = 0; i < count; i++)
+            {
+                matrix[i] = new int?[count];
+                for (int j = 0; j < count; j++)
+                {
+                    if (i == j) matrix[i][j] = 0; else matrix[i][j] = null;
+                }
+            }
+        }
+        public int NodeCount
+        {
+            get { return nodesCount; }
+        }
+        public int? GetElement(int i, int j)
+        {
+            if ((i>=matrix.Length)||(j>=matrix.Length))
+            {throw new System.IndexOutOfRangeException("Big number");}
+            return matrix[i][j];
+        }
+        public void SetElement(int i, int j, int weight)
+        {
+            if ((i >= matrix.Length) || (j >= matrix.Length))
+            { throw new System.IndexOutOfRangeException("Big number"); }
+            matrix[i][j] = weight;
+        }
+
+        public object Clone()
+        {
+            AdjacencyMatrix newMatrix = new AdjacencyMatrix(this.NodeCount);
+            for (int i = 0; i < this.matrix.Length; i++)
+            {
+                for (int j = 0; j < this.matrix[i].Length; j++)
+                {
+                    if (this.matrix[i][j].HasValue) { newMatrix.AddEdge(i, j, this.matrix[i][j].Value); }
+                }
+            }
+            return newMatrix;
+        }
+
+        void AddEdge(int start, int end, int weight)
+        {
+            if ((start>=matrix.Length)||(end>=matrix.Length))
+            {throw new System.IndexOutOfRangeException("Big number");}
+            matrix[start][end] = weight;
+        }
+
+        public override string  ToString()
+        {
+            string resultString = "";
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j].HasValue)
+                    resultString += matrix[i][j] + " "; else resultString += Char.ToString((char)(byte)236) + " "; 
+                }
+                resultString += String.Format("\n");
+            }
+            return resultString;
+        }
+    }
+}
---
+++ FloydWarshallAlgoritm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FloydWarshallAlgoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdjacencyMatrix matr = new AdjacencyMatrix(6);
+            matr.SetElement(0, 1, 2);
+            matr.SetElement(0, 3, 5);
+            matr.SetElement(1, 2, 4);
+            matr.SetElement(2, 3, 1);
+            matr.SetElement(2, 5, 7);
+            matr.SetElement(3, 0, -3);
+            matr.SetElement(3, 1, 4);
+            matr.SetElement(3, 2, 6);
+            matr.SetElement(3, 4, 5);
+            matr.SetElement(4, 2, 8);
+            matr.SetElement(4, 5, -1);
+            Algoritm alg = new Algoritm(matr);
+            Console.WriteLine(alg.ShowResult());
+            alg.AlgoritmStep();
+            Console.WriteLine(alg.ShowResult());
+            Console.ReadLine();
+        }
+    }
+}
