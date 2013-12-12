918593731596a34cf737c3b98abc34fd8121ab12
rem8086 <rem8086@gmail.com>
2013-12-12 15:26:20 +0400

    Add Shell Sort

--- FloydWarshallAlgoritm/Sort.cs
+++ FloydWarshallAlgoritm/Sort.cs
@@ -32,4 +32,29 @@ class Sort<T> where T:IComporable<T>
 		}
 		return arr;
 	}
+	
+	public T[] ShellSort(T[] arr)
+	{
+		int[] delta = {1, 4, 10, 23, 57, 132, 301, 701};
+		int deltaElement = delta.Length - 1;
+		while (delta[deltaElement] >= arr.Length) { deltaElement--; }
+		for (int i = deltaElement; i < 0; i--)
+		{
+			for (int j = 1; j <= delta[i]; j++)
+			{
+				for (int k = delta[i] + j - 1; k < arr[Length]; k += delta[i])
+				{
+					T key = arr[k];
+					int l = k - delta[i];
+					while ((l >= 0) && (arr[l].CompareTo(key) > 0))
+					{
+						arr[l+delta[i]] = arr[l];
+						l -= delta[i];
+					}
+					arr[l+delta[i]] = key;
+				}
+			}
+		}
+	}
+	
 }
